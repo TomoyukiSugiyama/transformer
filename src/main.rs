e868c13f56a5c4ce3569553d3cf8fa9ad3428232
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2026-05-03 22:23:14 +0900

    Sinusoidal Position Encording を追加

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,41 @@
-mod tokenizer;
 mod embedding;
-use crate::{embedding::Embedding, tokenizer::*};
+mod sinusoidal_pe;
+mod tokenizer;
+use crate::{embedding::Embedding, sinusoidal_pe::SinusoidalPE, tokenizer::*};
 
 fn main() {
     let corpus = vec!["Hello, world!", "Attention Is All You Need"];
 
     let tokenizer = Tokenizer::build(&corpus);
-    println!("Corpus: {:?}",corpus);
+    println!("Corpus: {:?}", corpus);
     println!("Vocab Size: {}", tokenizer.vocab_size());
 
     let mut token_ids = tokenizer.encode("unknown word");
     println!("Token Ids (Unknown Word): {:?}", token_ids);
-    
+
     token_ids = tokenizer.encode("Hello, world!Attention Is All You Need");
-    println!("Token Ids(Hello, world!Attention Is All You Need): {:?}", token_ids);
+    println!(
+        "Token Ids(Hello, world!Attention Is All You Need): {:?}",
+        token_ids
+    );
 
     let d_model = 4;
     let embedding = Embedding::new(tokenizer.vocab_size(), d_model);
 
-    let matrix = embedding.forward(&token_ids);
-    println!("Embedding matrix (seq_len={}, d_model={}):", token_ids.len(),d_model);
-    for (i, vec) in matrix.iter().enumerate() {
-        println!("[{i}] {:?}",vec);
+    let token_enb = embedding.forward(&token_ids);
+    println!(
+        "Embedding matrix (seq_len={}, d_model={}):",
+        token_ids.len(),
+        d_model
+    );
+    for (i, vec) in token_enb.iter().enumerate() {
+        println!("[{i}] {:?}", vec);
     }
 
+    let sin_pe = SinusoidalPE::new(512, d_model);
+    let output = sin_pe.forward(&token_enb);
+    println!("Sinusoidal Position Encording:");
+    for (i, o) in output.iter().enumerate() {
+        println!("[{i}] {:?}", o);
+    }
 }
---
+++ src/sinusoidal_pe.rs
@@ -0,0 +1,38 @@
+pub struct SinusoidalPE {
+    table: Vec<Vec<f32>>,
+    d_model: usize,
+}
+
+impl SinusoidalPE {
+    pub fn new(max_len: usize, d_model: usize) -> Self {
+        assert!(d_model % 2 == 0, "d_model must be even");
+        let table: Vec<Vec<f32>> = (0..max_len)
+            .map(|pos| {
+                (0..d_model)
+                    .map(|i| {
+                        let div = 10000_f32.powf((i / 2 * 2) as f32 / d_model as f32);
+                        let angle = pos as f32 / div;
+                        if i % 2 == 0 { angle.sin() } else { angle.cos() }
+                    })
+                    .collect()
+            })
+            .collect();
+        Self { table, d_model }
+    }
+
+    pub fn forward(&self, token_enb: &[Vec<f32>]) -> Vec<Vec<f32>> {
+        let seq_len = token_enb.len();
+        println!("seq_len: {seq_len}, teble_len={}",self.table.len());
+        assert!(seq_len <= self.table.len(), "seq_len exceeded max_len");
+        token_enb
+            .iter()
+            .enumerate()
+            .map(|(pos, enb)| {
+                enb.iter()
+                    .zip(self.table[pos].iter())
+                    .map(|(e, pe)| e + pe)
+                    .collect()
+            })
+            .collect()
+    }
+}
